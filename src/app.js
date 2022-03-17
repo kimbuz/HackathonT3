66411b1635a89aec6ba40232554f997b5566c53f
Juan Martin Bonelli <u555555@teco.com.ar>
2022-03-17 17:33:26 -0300

    Added Login Auth To Content

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ const app = express()
 //--------------------------------------------
 // Middlewares Load
 app.use(morgan('dev'))
+
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -114,7 +115,6 @@ app.get( base_url + '/docs', swaggerUi.setup(swaggerDocument, { explorer: true }
 //--------------------------------------------
 // Login Apis | No validation for Login
 app.use( base_url + '/users', userRoute )
-app.use( base_url + '/content', contentRoute )
 
 //--------------------------------------------
 // Link to Check Login OK!
@@ -124,8 +124,12 @@ app.get('/test', checkAuthenticated, (req,res) => {
 
 //--------------------------------------------
 // Default Route
+app.use( base_url + '/content', checkAuthenticated, contentRoute )
+
+//--------------------------------------------
+// Default Route
 app.get('*', (req,res) => {
-  res.redirect( '/login.html' )
+  res.status(404).json( { message: 'Not Available' } )
 })
 
 export default app
--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import app from './src/app.js'
+import mongoose from 'mongoose'
 
 console.log(`
 ===============================================================================
@@ -7,12 +8,8 @@ Welcome to Hackaton Backend
 ===============================================================================
 `)
 
-import mongoose from 'mongoose'
-
 const { MONGODB_USER, MONGODB_PASS, MONGODB_HOST, MONGODB_DATABASE } = process.env;
-
 const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}/${MONGODB_DATABASE}`
-
 mongoose.connect(MONGODB_URI)
         .then(db => console.log('database is connected'))
         .catch(err => console.log(err));
