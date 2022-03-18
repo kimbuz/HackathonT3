07fc5bfb8ea533e0cf7f5c20abbc7afeaa13abab
Juan Martin Bonelli <u555555@teco.com.ar>
2022-03-18 00:40:04 -0300

    Add Exception Catch Content

--- start.js
+++ start.js
@@ -10,9 +10,14 @@ Welcome to Hackaton Backend
 
 const { MONGODB_USER, MONGODB_PASS, MONGODB_HOST, MONGODB_DATABASE } = process.env;
 const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}/${MONGODB_DATABASE}`
+
 mongoose.connect(MONGODB_URI)
-        .then(db => console.log('database is connected'))
-        .catch(err => console.log(err));
+    .then(db => console.log('database is connected'))
+    .catch(err => {
+        console.error('Server missing Database Config Mongo Env.')
+        //console.error(err)
+        process.exit(1)
+    });
 
 const PORT = process.env.PORT || 8080
 
